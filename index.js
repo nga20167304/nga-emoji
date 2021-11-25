837d095a3b23a8c9a3ef48f8e8cecf953010abbc
Nga <38340962+nga20167304@users.noreply.github.com>
2021-11-25 19:33:15 +0700

    fix commented

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-module.exports = async function displayEmoji(){
-  let emoji = require('emoji.json')
+let emoji = require('emoji.json')
 
-  function getNameEmoji(){
-    let emoji_name = [];
-    for(i=0; i<emoji.length; i++){
-      emoji_name.push(emoji[i].name)
-    }
-    return emoji_name;
+function getNameEmoji() {
+  let emoji_name = [];
+  for (i = 0; i < emoji.length; i++) {
+    emoji_name.push(emoji[i].name)
   }
-
-  async function displayNameEmoji(){
-    const { AutoComplete } = require('enquirer');
-
-    const prompt = new AutoComplete({
-      name: 'emoji',
-      message: 'Pick your favorite emoji',
-      limit: 10,
-      initial: 3,
-      choices: getNameEmoji()
-    });
-
-    await prompt.run()
-      .then(answer => {
-        for(i=0; i<emoji.length; i++){
-          if(emoji[i].name == answer){
-            console.log("Your favorite emoji is",emoji[i].char);
-          }
-        }
-      })
-      .catch(console.error);
-  }
-  displayNameEmoji();
+  return emoji_name;
 }
+
+async function displayNameEmoji() {
+  const { AutoComplete } = require('enquirer');
+
+  const prompt = new AutoComplete({
+    name: 'emoji',
+    message: 'Pick your favorite emoji',
+    limit: 10,
+    initial: 3,
+    choices: getNameEmoji()
+  });
+
+  await prompt.run()
+    .then(answer => {
+      for (i = 0; i < emoji.length; i++) {
+        if (emoji[i].name == answer) {
+          console.log("Your favorite emoji is", emoji[i].char);
+        }
+      }
+    })
+    .catch(console.error);
+}
+displayNameEmoji();
